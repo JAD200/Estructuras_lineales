d0a402ce3e958379b8413e7f2d0063f13b6c9b7e
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-01 14:26:33 -0300

    Double linked list

---
+++ double_linked_list.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class TwoWayNode(Node):
+    def __init__(self, data, previous=None, next=None):
+        Node.__init__(self, data, next)
+        self.previous = previous
