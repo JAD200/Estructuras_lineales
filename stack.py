0b4f937bea0de864e8f1aefa51892406f201dfde
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-06 11:50:27 -0300

    Add -- Comments to stack

--- stack.py
+++ stack.py
@@ -7,21 +7,32 @@ class Stack():
         self.size = 0
 
     def push(self, data):
-        node = Node(data)
+        """push Adds an element on top of the stack
 
+        Args:
+            data: Data to be added
+        """
+        node = Node(data)
+        #   Puts the data at the top of the stack
         if self.top:
             node.next = self.top
             self.top = node
-        else:
+        else:  # Else makes the data the new node
             self.top = node
 
         self.size += 1
 
     def pop(self):
+        """pop Removes an element at the top of the stack
+
+        Returns:
+            data: Data removed from the stack
+        """
+        #   If there is data on the stack removes the data at the top
         if self.top:
             data = self.top.data
             self.size -= 1
-
+            #   In case there is more data on the stack
             if self.top.next:
                 self.top = self.top.next
             else:
@@ -33,11 +44,34 @@ class Stack():
             return 'The stack is empty'
 
     def peek(self):
+        """peek Shows the  data of the top of the stack
+
+        Returns:
+            data: Data at the top
+        """
         if self.top:
             return self.top.data
         else:
             return 'The stack is empty'
 
     def clear(self):
+        """clear Empties the stack
+        """
         while self.top:
             self.pop()
+
+
+""" Code use in the shell to test the code
+from stack import Stack
+food = Stack()
+food.push('egg')
+food.push('ham')
+food.push('spam')
+food.pop()
+#?  Expected:   'spam'
+food.peek()
+#?  Expected:   'ham'
+food.clear()
+food.peek()
+#?  Expected:   'The stack is empty'
+"""
