45f2fd31b4b456ae50951b85210e76d91ccca192
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-07 13:37:25 -0300

    List based queue

---
+++ module5_queues/list_based_queue.py
@@ -0,0 +1,33 @@
+
+
+class ListQueue:
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def enqueue(self, data):
+        self.items.insert(0, data)
+        self.size += 1
+
+    def dequeue(self):
+        data = self.items.pop()
+        self.size -= 1
+        return data
+
+    def traverse(self):
+        total_items = self.size
+
+        for item in range(total_items):
+            print(self.items[item])
+
+""" Code use in the shell to test the code
+from list_based_queue import ListQueue
+food = ListQueue()
+food.enqueue('eggs')
+food.enqueue('ham')
+food.enqueue('spam')
+food.dequeue()
+#?  Expected:   'eggs'
+food.traverse()
+#?  Expected:   spam ham
+"""
